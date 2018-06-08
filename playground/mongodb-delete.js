f6d32dd96822378e2bd2a4619c3d73a33151c6d5
Anurag <anuraganand2171@gmail.com>
2018-06-08 21:59:25 +0530

    new delete commit

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,13 @@
+const {MongoClient,ObjectID} = require("mongodb");
+MongoClient.connect("mongodb://localhost:27017/TodoApp",(error,client)=>{
+    if(error){
+        return console.log("unable to connect to MongoDB server");
+    }
+    console.log("Connected to MongoDB");
+    const db = client.db("TodoApp");
+
+    db.collection("Todos").deleteMany({text:"get out of cz"}).then((result)=>{
+        console.log(result);
+    });
+
+});
